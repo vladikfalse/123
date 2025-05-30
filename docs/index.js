c310415a84a8fc8472b81c19b51a27c39aea7200
vladikfalse <vladert224@gmail.com>
2025-05-30 13:32:57 +0300

    chat-main

--- docs/index.js
+++ docs/index.js
@@ -18,6 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Це клік у бекдроп");
   }
 });
+ 
+(() => {
+    document
+      .querySelector('.js-speaker-form')
+      .addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        new FormData(e.currentTarget).forEach((value, name) => {
+          console.log(`${name}: ${value}`);
+        });
+      });
+  })();
+
+
+
+
 
 
 
